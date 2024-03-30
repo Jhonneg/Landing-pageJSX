0331a5966a6ad58208dcf03488cb9f12e08a04a4
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-03-30 06:14:40 -0300

    added hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,43 +1,35 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "../components/Cardlist.jsx";
 import SearchBox from "../components/SearchBox.jsx";
 import Scroll from "../components/Scroll.jsx";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => setRobots(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter((robots) => {
-      return robots.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <main className="text-center">
-        <h1 className="text-8xl mt-3 font-w font-bold">Robo Friends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </main>
-    );
-  }
-}
 
+  const filteredRobots = robots.filter((robots) => {
+    return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <main className="text-center">
+      <h1 className="text-8xl mt-3 font-w font-bold">Robo Friends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </main>
+  );
+}
 export default App;
