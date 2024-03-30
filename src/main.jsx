16afeefca5fec27e9ce32aadcf29c5de8205062a
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-03-29 23:57:57 -0300

    added searchbox

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CardList from "./Cardlist";
+import App from "./App"
 import "./index.css";
-import { robots } from "./robots.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App/ >
   </React.StrictMode>
 );
