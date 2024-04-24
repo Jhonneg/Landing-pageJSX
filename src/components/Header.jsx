66797159e16caba1d23a39a1f5881035b9dcb01b
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-04-24 16:45:51 -0300

    changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,12 @@ import { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <h1 className="text-8xl mt-3 font-w font-bold">Robo Friends</h1>;
+    return (
+      <div>
+        <h1 className="text-8xl mt-3 font-w font-bold">Robo Friends</h1>
+        <h1 className="text-8xl mt-3 font-w font-bold">Robo Friends</h1>
+        <h1 className="text-8xl mt-3 font-w font-bold">Robo Friends</h1>
+      </div>
+    );
   }
 }
