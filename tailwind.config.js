4b0dfc8959942b311050f6943ffef0a582f02290
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-04-28 02:02:02 -0300

    fixed styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/components/main.jsx",
-    "./src/components/Card.jsx",
-    "./src/components/SearchBox.jsx",
-    "./src/containers/App.jsx",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
